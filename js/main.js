4d94c008a5287980d9942286add666f9a94191a9
maru <k_maru29@yahoo.co.jp>
2025-11-21 23:59:54 +0900

    kyoko11 Voice-sectionの追加（動画本体は除外）

--- js/main.js
+++ js/main.js
@@ -1,20 +1,47 @@
 $(function () {
 
-// Q&A
+  //Voice
+  $('#voice .slide-items').slick({
+    infinite: true,           //スライダーのループ
+    centerPadding: '20px',    // ★真ん中を広く見せる
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    // ★ centerMode / centerPadding を全部消す
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2, 
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1 ,
+        }
+      }
+    ]
+  });
 
-    $(".openbtn1").click(function () {
-        $(".close-text").css("visibility", "visible");
-        $(this).parent(".close-text").css("visibility", "hidden");
-        $(".open-text").css("visibility", "hidden");
-        $(this).parents(".content").find(".open-text").css("visibility", "visible");
-    });
 
-    $(".openbtn2").click(function () {
-        $(this).parents(".open-text").css("visibility", "hidden");
-    });
 
-     // クリックの処理はそのままでOK
-  
+  // Q&A
+
+  $(".openbtn1").click(function () {
+    $(".close-text").css("visibility", "visible");
+    $(this).parent(".close-text").css("visibility", "hidden");
+    $(".open-text").css("visibility", "hidden");
+    $(this).parents(".content").find(".open-text").css("visibility", "visible");
+  });
+
+  $(".openbtn2").click(function () {
+    $(this).parents(".open-text").css("visibility", "hidden");
+  });
+
+  // クリックの処理はそのままでOK
+
   // スクロールでふきだし表示
   $(window).on("scroll", function () {
 
